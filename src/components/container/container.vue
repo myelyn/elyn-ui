3c7783ce350d479bbe6b63802aae77d6cfdbd70c
myelyn <myelyn@163.com>
2022-03-16 17:41:22 +0800

    fix: Container component name case problem fix

--- src/components/container/container.vue
+++ src/components/container/container.vue
@@ -28,7 +28,7 @@ const isVertical = computed(() => {
     return slots.default().some((vn:VNode) => {
       const tag = (vn.type as Component).name;
       console.log(tag)
-      return tag === 'yHeader' || tag === 'yFooter';
+      return tag === 'YHeader' || tag === 'YFooter';
     });
   }
   return props.direction === 'vertical';
